61f1cc467a853c4a9361ff677004fc93ff51f088
hsuk <sukhyunil@naver.com>
2022-09-22 13:53:42 +0900

    commit

--- lib/ui/dice_screen.dart
+++ lib/ui/dice_screen.dart
@@ -1,13 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class DiceScreen extends StatelessWidget {
-  const DiceScreen({Key? key}) : super(key: key);
+class DiceScreen extends StatefulWidget {
+  @override
+  State<DiceScreen> createState() => _DiceScreenState();
+}
 
-  // 주사위 숫자 2개를 랜덤하게 생성
-  void _rollDice(BuildContext context) {
-    // final snackBar = SnackBar(content: Text('주사위 클릭...'));
-    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    
+class _DiceScreenState extends State<DiceScreen> {
+
+  // state..
+  int _firstDiceNumber = 1;
+  int _secondDiceNumber = 2;
+
+  void _rollDice() {
+    // 주사위 숫자 2개를 랜덤하게 생성
+    Random random = Random();
+    setState(() {
+      _firstDiceNumber = random.nextInt(6) + 1;
+      _secondDiceNumber = random.nextInt(6) + 1;
+    });
+
+    print('dice number $_firstDiceNumber, $_secondDiceNumber');
   }
 
   @override
@@ -17,14 +31,14 @@ class DiceScreen extends StatelessWidget {
           children: [
             Expanded(
              child: TextButton(
-                 child: Image.asset('images/dice3.png'),
-               onPressed: () => { _rollDice(context) },
+                 child: Image.asset('images/dice$_firstDiceNumber.png'),
+               onPressed: () => { _rollDice() },
              ),
         ),
         Expanded(
             child: TextButton(
-                child: Image.asset('images/dice5.png'),
-              onPressed: (){ _rollDice(context); },
+                child: Image.asset('images/dice$_secondDiceNumber.png'),
+              onPressed: (){ _rollDice(); },
             ),
         ),
       ]),
